2bae3477e1e2c350adbc4b80e6ceb23d5c03115e
eric.zhang <ezhang2@stuy.edu>
2017-12-15 11:13:12 -0500

    removed bug-fixing messages

--- pipe_networking.c
+++ pipe_networking.c
@@ -14,15 +14,15 @@ int server_handshake(int *to_client) {
   mkfifo(FIFO, 0644);
   int FIFO1 = open(FIFO, O_RDONLY);
   char from_client[50];
-  printf("Waiting for message from client\n");
+  //printf("Waiting for message from client\n");
   read(FIFO1, from_client, 50);
-  printf("%s\n", from_client);
+  //printf("%s\n", from_client);
   *to_client = open(from_client, O_WRONLY);
   write(*to_client, "confirmation\n", 50);
   if(0 >= read(FIFO1, from_client, 50)){
     printf("no confirmation from client\n");
   }
-  else printf("%s\n", from_client);
+  //else printf("%s\n", from_client);
   unlink(FIFO);
   return FIFO1;
 }
@@ -40,13 +40,13 @@ int server_handshake(int *to_client) {
 int client_handshake(int *to_server) {
   *to_server = open(FIFO, O_WRONLY);
   mkfifo("Yes?", 0644);
-  printf("Sending FIFO name to sever\n");
+  //printf("Sending FIFO name to sever\n");
   write(*to_server, "Yes?", 5);
   int FIFO2 = open("Yes?", O_RDONLY);
   char from_server[50];
-  printf("Waiting for message from server\n");
+  //printf("Waiting for message from server\n");
   read(FIFO2, from_server, 50);
-  printf("%s\n", from_server);
+  //printf("%s\n", from_server);
   write(*to_server, "confirmation\n", 50);
   unlink("Yes?");
   return FIFO2;
